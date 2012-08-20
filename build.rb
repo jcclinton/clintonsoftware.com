fd4cf8d511ac47c937db60358e95efba5ee80ae0
Jamie Clinton <jcclinton24@gmail.com>
2012-08-20 14:11:05 -0700

    got build script working

--- build.rb
+++ build.rb
@@ -9,6 +9,24 @@ pages.each do |page|
 	contents = file.read
 
 	html = wrapper.gsub /%HTML%/, contents
+	list = File.open("_templates/_list.html", 'rb').read
+	list_html = ''
+
+	pages.each do |p|
+		if p == page
+			sub = 'active'
+		else
+			sub = ''
+		end
+
+		sub_html = list.gsub /%CLASS%/, sub
+		sub_html.gsub! /%NAME%/, "#{p}.html"
+		sub_html.gsub! /%NAME_CAP%/, "#{p.capitalize}"
+
+		list_html = "#{list_html}#{sub_html}"
+	end
+
+	html.gsub! /%LIST%/, list_html
 
 	File.open "#{page}.html", 'w' do |f|
 		f.write html
