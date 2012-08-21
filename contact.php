8947f4acd3fd790838a9f0424add3d920ba2e30a
Jamie Clinton <jcclinton24@gmail.com>
2012-08-20 21:52:15 -0700

    added validity checking to email

--- contact.php
+++ contact.php
@@ -6,9 +6,17 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
+if( nameCheck($name, $email) ){
+	die('Error: illegal newline characters in email address');
+}
+
+if( !validityCheck($email) ){
+	die('Error: invalid email address');
+}
+
 $formcontent="From: $name \n Address: $email \n Message: $message";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
-mail($myEmail, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+mail($myEmail, $subject, $formcontent, $mailheader) or die("Error: Please try again.");
+echo "Message Sent, Thank You!";
 ?>
